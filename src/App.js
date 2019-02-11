a3c2c35ba363e4d6f85026445c82dc950869e234
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-11 17:59:39 -0500

    create function to calculate the borders of the face to draw the box

--- src/App.js
+++ src/App.js
@@ -29,9 +29,24 @@ class App extends Component {
     super();
     this.state = {
       input: "",
-      imageUrl: ""
+      imageUrl: "",
+      box: {}
     };
   }
+
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width;
+      topRow: clarifaiFace.top_row * height;
+      rightCol: width - (clarifaiFace.right_col * width);
+      bottomRow: height - (clarifaiFace.right_col * height);
+    }
+  };
 
   onInputChange = event => {
     this.setState({ input: event.target.value });
@@ -39,16 +54,10 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err));
   };
 
   render() {
