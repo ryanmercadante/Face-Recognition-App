28db52b172bed03c3e94385ff1475d9dc15687fb
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-11 15:17:30 -0500

    add particles react js for cool effect

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
+import Particles from "react-particles-js";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
