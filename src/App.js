502f6e1888ac47c0df969995c50cd1d9f7985561
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-11 18:15:45 -0500

    create function to display the box over the face

--- src/App.js
+++ src/App.js
@@ -41,11 +41,16 @@ class App extends Component {
     const width = Number(image.width);
     const height = Number(image.height);
     return {
-      leftCol: clarifaiFace.left_col * width;
-      topRow: clarifaiFace.top_row * height;
-      rightCol: width - (clarifaiFace.right_col * width);
-      bottomRow: height - (clarifaiFace.right_col * height);
-    }
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({ box: box });
   };
 
   onInputChange = event => {
@@ -56,7 +61,9 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => this.calculateFaceLocation(response))
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
       .catch(err => console.log(err));
   };
 
@@ -71,7 +78,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
