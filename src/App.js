f7194783f349e45c730800b0755a5b608dfaa3d9
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-19 01:03:31 -0500

    move clarifai api key and call to backend

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ import Signin from "./components/Signin/Signin";
 import Register from "./components/Register/Register";
 import "./App.css";
 import Particles from "react-particles-js";
-import Clarifai from "clarifai";
-
-const app = new Clarifai.App({
-  apiKey: "5bf5bdd1b8b4496d966161140f3d5672"
-});
 
 const particlesOptions = {
   particles: {
@@ -83,8 +78,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      fetch("http://localhost:3000/imageurl", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch("http://localhost:3000/image", {
